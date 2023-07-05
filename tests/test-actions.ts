ea5bd45a24bf02ed81f51ec64c669320289fa65b
Jason Reed <jcreed@gmail.com>
2023-07-05 09:00:25 -0400

    [Tests] Improve state creation in test-actions.ts (fix #39)

--- tests/test-actions.ts
+++ tests/test-actions.ts
@@ -1,28 +1,18 @@
-import { Skill, State, initState } from '../src/state';
-import { Action, doAction } from '../src/action';
+import { State, initState } from '../src/state';
+import { Action, doAction, enterInboxMenu } from '../src/action';
 
 describe('edit form action', () => {
   it('should work on envelope', () => {
 
     const state: State = initState();
-    state.items_ = {
-      '0': { t: 'envelope', address: '', contents: [], size: 3 }
-    };
-    state.itemLocs_ = { '0': { t: 'inbox', ix: 0 } };
-    state.uiStack = [
-      { t: 'menu', which: { t: 'inbox' }, ix: 0 },
-      { t: 'menu', which: { t: 'main' }, ix: 2 }
-    ];
-    state.idCounter = 1;
-    state.inv.inbox_ = [{ 'unread': false, 'id': 0 }];
-
-    const action: Action = {
-      t: 'editForm',
-      'form': { 't': 'Envelope Address' }, 'id': 0, 'saveCont': { 't': 'envelope' }
-    };
-
-    doAction(state, action);
-    const lastFrame = state.uiStack[0];
-    expect(lastFrame.t).toEqual('editForm');
+    doAction(state, { t: 'addItems', items: [{ unread: false, item: { t: 'envelope', address: '', contents: [], size: 3 } }] });
+    doAction(state, enterInboxMenu());
+    doAction(state,
+      {
+        t: 'editForm',
+        'form': { 't': 'Envelope Address' }, 'id': 0, 'saveCont': { 't': 'envelope' }
+      });
+    expect(state.uiStack[1]).toEqual({ t: 'menu', which: { t: 'inbox' }, ix: 0 });
+    expect(state.uiStack[0].t).toEqual('editForm');
   });
 });
